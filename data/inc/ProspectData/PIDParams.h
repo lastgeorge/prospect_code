00cef8265d5603b7549d6e1d9fc64a79d8fbb9f9
Xin Qian <last.qx@gmail.com>
2018-05-29 15:13:57 -0400

    update code

---
+++ data/inc/ProspectData/PIDParams.h
@@ -0,0 +1,17 @@
+namespace Prospect{
+  class PIDParams {
+    // high energy muon ...
+
+    // veto ...
+    
+    // high energy neutron ... 
+
+    // veto ...
+    
+    // prompt signal
+
+    // delay signal
+    
+    
+  };
+}
